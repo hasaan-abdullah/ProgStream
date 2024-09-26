addc533ab443d36b11680b02e7009e0fdb18144e
Hasaan Abdullah <127670733+hasaan-abdullah@users.noreply.github.com>
2024-09-27 02:26:28 +0500

    Update route.ts

--- app/api/calculate/route.ts
+++ app/api/calculate/route.ts
@@ -12,22 +12,22 @@ export const POST = async (request: Request) => {
   const result = { boxes: 0, packs: 0, bottles: 0 };
   let remainingBottles = requiredBottles;
 
-  // First calculate the number of boxes
+  
   if (remainingBottles >= pieces[2]) {
     result.boxes = Math.floor(remainingBottles / pieces[2]);
     remainingBottles %= pieces[2];
   }
 
-  // Then calculate the number of packs
+
   if (remainingBottles >= pieces[1]) {
     result.packs = Math.floor(remainingBottles / pieces[1]);
     remainingBottles %= pieces[1];
   }
 
-  // Finally, the remaining bottles
+
   result.bottles = remainingBottles;
 
-  // Calculate the total price
+
   const totalPrice =
     result.boxes * prices[2] + result.packs * prices[1] + result.bottles * prices[0];
 
